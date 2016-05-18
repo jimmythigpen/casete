c4b51db624fd6464530df8ecf6a0239cf47bb937
Jimmy Thigpen <jmthigpen@me.com>
2016-05-17 21:50:03 -0500

    Update methods

--- server/methods/users.js
+++ server/methods/users.js
@@ -1,29 +1,6 @@
 Meteor.methods({
 
-  // * * * * * * UPDATE * * * * * *`
   'users.updateFromFacebook' () {
-    const user = Meteor.user();
-    // const valuesFromGoogle = _.pick(user.services.google, 'given_name', 'family_name', 'picture');
-    // const { given_name: firstName, family_name: lastName, picture: profilePicture } = valuesFromGoogle;
-
-    // Users.update(Meteor.userId(), {
-    //   $set: {
-    //     'profile.firstName': firstName,
-    //     'profile.lastName': lastName,
-    //     'profile.profilePicture': profilePicture,
-    //   }
-    // });
-
-    // return true;
-  },
-
-  'users.updatePreferences' (obj = {}) {
-    Users.update(Meteor.userId(), {
-      $set: {
-        'profile.canDrive': obj.canDrive,
-        'profile.availableSeats': obj.availableSeats,
-        'profile.hasSetPreferences': true,
-      },
-    });
+    // add facebook photo
   },
 });
