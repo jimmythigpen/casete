1343ba7fa22ea66035bdfabeae58cdbd5ae7d0fe
Jimmy Thigpen <jmthigpen@me.com>
2016-05-24 16:29:44 -0500

    Update marker for maps

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -4,6 +4,7 @@ import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 import { Login } from '../../ui/login.jsx';
 import { App } from '../../ui/app.jsx';
 import { Index } from '../../ui/index.jsx';
+import { Profile } from '../../ui/profile.jsx';
 import { NotFound } from '../../ui/notfound.jsx';
 
 Meteor.startup( () => {
@@ -12,7 +13,8 @@ Meteor.startup( () => {
       <Route path="/login" component={ Login }>
     </Route>
     <Route path="/" component={ App }>
-      <IndexRoute component={ Index  }/>
+      <IndexRoute component={ Index }/>
+       <Route path="/profile" component={ Profile } />
       <Route path="*" component={ NotFound } />
     </Route>
     </Router>,
--- imports/ui/index.jsx
+++ imports/ui/index.jsx
@@ -2,31 +2,64 @@ import React from 'react';
 
 
 export class Index extends React.Component {
+  currentLocation() {
+   Tracker.autorun(() => {
+    const currentLocation = Geolocation.latLng();
+    if (currentLocation) {
+      alert(`Lat: ${currentLocation.lat.toFixed(2)} // Lng: ${currentLocation.lng.toFixed(2)}`)
+    }
+   });
+  }
+
   componentWillMount() {
     GoogleMaps.load({ key: 'AIzaSyAJYsUQJGknptrOcD43JztqX3hF8TgvjSc' });
-  };
-  componentDidMount() {
+
     Tracker.autorun(() => {
       if (GoogleMaps.loaded()) {
-        console.log('loaded!');
+        console.log('google maps loaded!');
         GoogleMaps.create({
           name: 'caseteMap',
           element: document.getElementById('map-container'),
             options: {
             center: new google.maps.LatLng(29.7614528, -95.4791437),
-            zoom: 10
+            zoom: 10,
           }
         });
       }
     })
   };
+  componentDidMount() {
+    GoogleMaps.ready('caseteMap', function(map) {
+       Tracker.autorun(() => {
+        let marker;
+        const zoom = 20;
+        const latLng = Geolocation.latLng()
+        if (latLng) {
+          if (!marker) {
+            marker = new google.maps.Marker({
+              position: new google.maps.LatLng(latLng.lat, latLng.lng),
+              map: map.instance,
+            });
+          } else {
+            marker.setPosition(latLng);
+          }
+          map.instance.setCenter(marker.getPosition());
+          map.instance.setZoom(zoom);
+        } 
+     
+      });
+    });
+  };
+
   render() {
     return (
       <div>
         <h1>Index</h1>
+        <div>{this.currentLocation()}</div>
         <div id="map-container"></div>
         {this.props.children}
       </div>
     )
   }
 }
+  
